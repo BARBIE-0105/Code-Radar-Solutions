f353398b78b9af9ad90215f6d227bc17afaef160
Barbie <barbie.ynr@gmail.com>
2025-02-18 08:04:53 +0530

    Solution to Check Greater Than

--- Check Greater Than@@67882a731b4392d8fc1577d6/code.c
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -5,11 +5,11 @@ int main()
     scanf("%d%d",&a,&b);
     if(a > b)
     {
-    printf("%d",1);
+    printf("%d",a);
     }
     else
     {
-    printf("%d",0);
+    printf("%d",b);
     }
     return 0;
 }
