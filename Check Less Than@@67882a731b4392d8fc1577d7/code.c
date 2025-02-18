6bfd88e7241aec8045a64269af4c9bdf8a7f0b39
Barbie <barbie.ynr@gmail.com>
2025-02-18 08:17:55 +0530

    Solution to Check Less Than

--- Check Less Than@@67882a731b4392d8fc1577d7/code.c
+++ Check Less Than@@67882a731b4392d8fc1577d7/code.c
@@ -5,11 +5,11 @@ int main()
     scanf("%d%d",&a,&b);
     if(a<b)
     {
-        printf("%d",a);
+        printf("True",a);
     }
     else
     {
-        printf("%d",b);
+        printf("False",b);
     }
     return 0;
 }
